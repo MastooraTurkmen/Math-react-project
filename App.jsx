b871e8d151a48749ac25809c98cd3f14e0141cda
Mastoora Turkmen <mastooraturkmen@gmail.com>
2023-11-25 09:04:55 +0430

    removing comments

--- App.jsx
+++ App.jsx
@@ -179,27 +179,6 @@ export default function App() {
         } 
     }
     
-/*  Challenge
-
-        The app's current UX is bad because elements don't appear or disappear when they should; they're all lumped together. Your task is to improve this as follows: 
-        
-            1. The <h1> element should only be rendered before the game has started. It should not 
-               be rendered afterward. 
-               
-            2. The <div> with the className "problem-container" should only be rendered if the game 
-               has started. Before that, it should not be rendered. 
-               
-            3. The buttons stored in the variable gamePlayButtons should only be rendered if the 
-               game has started. Otherwise, they should not be rendered, and the button stored in the variable startButton should appear instead.  
-            
-            4. The string stored in the answerStatus variable should only appear when the user has 
-               submitted an answer, and it should also disappear after 2 seconds. 
-               
-        Tip: You can invoke showStates() anywhere in the top level of this component and play around with the app to familiarize yourself with the app's states and how they change as the user interacts with it. Refer to the slides to get a better idea of how the app should react to the user's actions. 
-               
-        Note: To complete the challenge, you *only* have to write a little code inside of the return statement below. There is no other code you have to write in this or any other file! 
-*/
-    
     return (
         <div className="wrapper">  
      
